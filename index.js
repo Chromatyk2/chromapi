6c0e2b6b33eec8767f453576e010b9e66fa12eb7
pierre schuvey <chromatykretro@gmail.com>
2024-11-24 18:41:25 +0100

    fix

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var connection = mysql.createConnection({
     host     : process.env.MYSQL_ADDON_HOST,
     database : process.env.MYSQL_ADDON_DB,
     user     : process.env.MYSQL_ADDON_USER,
-    password : process.env.MYSQL_ADDON_PASSWORD
+    password : process.env.MYSQL_ADDON_PASSWORD,
+    port : process.env.MYSQL_ADDON_PORT
 });
 connection.connect(function(err) {
     if (err) throw err;
