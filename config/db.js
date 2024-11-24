82edef06c46e2ba22fec15ce7ee4b266872e5fad
pierre schuvey <chromatykretro@gmail.com>
2024-11-24 20:19:24 +0100

    fix

--- config/db.js
+++ config/db.js
@@ -1,15 +1,16 @@
-const mysql = require('mysql')
 const process = require('process')
+var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : process.env.MYSQL_ADDON_HOST,
   database : process.env.MYSQL_ADDON_DB,
   user     : process.env.MYSQL_ADDON_USER,
   password : process.env.MYSQL_ADDON_PASSWORD
 });
+
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
   if (error) throw error;
   console.log('The solution is: ', results[0].solution);
 });
 
 
-module.exports = pool;
+module.exports = connection;
