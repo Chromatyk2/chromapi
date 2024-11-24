a7fc6c93f87837222761b85e19ee981fc23abaa3
pierre schuvey <chromatykretro@gmail.com>
2024-11-24 19:59:15 +0100

    fix

--- index.js
+++ index.js
@@ -9,14 +9,8 @@ var connection = mysql.createConnection({
     password : process.env.MYSQL_ADDON_PASSWORD
 });
 connection.connect()
-connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-    if (err) throw err
-
-    console.log('The solution is: ', rows[0].solution)
-})
 
 app.get("/api/getProfil/:pseudo", (req, res, next)=>{
-
     const pseudo = req.params.pseudo;
     connection.query("SELECT pseudo, xp, first_pokemon, second_pokemon, third_pokemon, fourth_pokemon, fifth_pokemon, sixth_pokemon, profil_picture, level, box, canOpen, lastOpening, pkmToken FROM profil WHERE pseudo = 'chromatyk'", pseudo,
         (err,result)=>{
@@ -26,7 +20,6 @@ app.get("/api/getProfil/:pseudo", (req, res, next)=>{
             res.send(result)
         });
 });
-connection.end()
 
 app.listen(8080, () => {
     console.log('Server started on port 3000');
