3181c9a78350bda4a472ffd4b188b3fc581048ea
pierre schuvey <chromatykretro@gmail.com>
2024-11-24 17:00:36 +0100

    fix

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ var connection   = mysql.createConnection({
     host: "bqg2wximjlnqgmfjj44o-mysql.services.clever-cloud.com",
     user: "u1lw03a0o85nt1dw",
     password: "5Q9BxiCFQgxHyBpg67eE",
-    database:"bqg2wximjlnqgmfjj44o"
+    database:"bqg2wximjlnqgmfjj44o",
+    port:21610
 });
-connection.connect()
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:', err);
+        return;
+    }
+    console.log('Connected to MySQL database');
+});
 connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
     if (error) throw error;
     console.log('The solution is: ', results[0].solution);
