235af570c90745c0f20b0adc79c59f248f8957e5
pierre schuvey <chromatykretro@gmail.com>
2024-11-24 19:48:37 +0100

    fix

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ var connection = mysql.createConnection({
     user     : process.env.MYSQL_ADDON_USER,
     password : process.env.MYSQL_ADDON_PASSWORD
 });
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+connection.connect()
+connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+    if (err) throw err
+
+    console.log('The solution is: ', rows[0].solution)
+})
+connection.end()
+
 app.listen(8080, () => {
     console.log('Server started on port 3000');
 });
