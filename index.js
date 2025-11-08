ebd5232cedeb61f063681de349d2fc9f2c1cb2b5
pierre schuvey <chromatykretro@gmail.com>
2025-11-08 03:54:41 +0100

    fix

--- index.js
+++ index.js
@@ -1184,7 +1184,7 @@ app.get("/api/getProfil/:pseudo", (req, res, next)=>{
 app.get("/api/getProfilByPseudo/:pseudo", (req, res, next)=>{
 
     const pseudo = req.params.pseudo;
-    db.query("SELECT pseudo, xp, first_pokemon, second_pokemon, third_pokemon, fourth_pokemon, fifth_pokemon, sixth_pokemon, profil_picture, level, box, canOpen, lastOpening, pkmToken, badge, powder, berry FROM profil WHERE pseudo = ?", pseudo,
+    db.query("SELECT pseudo, xp, first_pokemon, second_pokemon, third_pokemon, fourth_pokemon, fifth_pokemon, sixth_pokemon, profil_picture, level, box, canOpen, lastOpening, pkmToken, badge, powder, berry, firstNeg, secondNeg,thirdNeg,fourthNeg,fiveNeg,sixthNeg FROM profil WHERE pseudo = ?", pseudo,
         (err,result)=>{
             if(err) {
                 console.log(err)
