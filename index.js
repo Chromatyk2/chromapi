76f2259ddb0293ce4c47ed5acc7781c75d1538a6
pierre schuvey <chromatykretro@gmail.com>
2026-06-28 12:25:16 +0200

    Page Nostalpick

--- index.js
+++ index.js
@@ -2403,13 +2403,13 @@ app.post(
                     ball: 0.10,
                     great: 0.50,
                     ultra: 1.00,
-                    safari:0.50
+                    safari:0.40
                 },
                 2: {
                     ball: 0.10,
                     great: 0.30,
                     ultra: 0.75,
-                    safari: 0.30
+                    safari: 0.20
                 },
                 3: {
                     ball: 0.05,
